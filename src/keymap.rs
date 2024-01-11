1fcbc202803315a4dfbe3c0502e11e482ae692bc
Vadim Kolontsov <vadim@kolontsov.com>
2024-01-12 01:31:32 +0200

    add m8c keymap and a special key to change keymap

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,15 +1,23 @@
 use crate::Layers;
-use crate::{RESET, UF2};
+use crate::{RESET, UF2, CHANGE_DEFAULT_LAYER};
 
 pub static LAYERS: Layers = keyberon::layout::layout! {
+    // layer 0: m8.run keys
     {
         [      Up     Z  X  ],
         [ Left Down   Right ],
-        [ (1)  LShift Space ] ,
+        [ (2)  LShift Space ] ,
     }
+    // layer 1: m8c keys
     {
-        [    {RESET}  t t   ],
-        [ n  n        {UF2} ],
-        [ t  t        t     ],
+        [      Up     A  S  ],
+        [ Left Down   Right ],
+        [ (2)  LShift Space ] ,
+    }
+    // layer 2: special layer (activated by Left+Down chord)
+    {
+        [    t t {UF2} ],
+        [ n  n {CHANGE_DEFAULT_LAYER} ],
+        [ t  t {RESET} ],
     }
 };
